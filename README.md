6f941a18d7bdd57eca985021a868eff6f1ff635a
Oksana <oksanasurotiyk@gmail.com>
2020-06-18 14:08:56 +0300

    add test to file README

--- README.md
+++ README.md
@@ -11,3 +11,17 @@
 ## Links
 
 https://vagusx.github.io/you-dont-need-selenium/
+
+TEST PLAN
+Testing search buttons on the tab Active
+
+- click on Active
+- click on search field
+- in the search field enter the name of one of the tasks
+- expect to see task which we type in the field
+
+Testing plus button
+
+- click on Active
+- click plus button
+- excpect to see that the input field disappears
--- src/index.spec.js
+++ src/index.spec.js
@@ -9,9 +9,6 @@ jest.setTimeout(30000); // because when headless: false, it takes longer
 const ENTER = String.fromCharCode(13);
 const TAB = String.fromCharCode(9);
 
-const ENTER = String.fromCharCode(13);
-const TAB = String.fromCharCode(9);
-
 // The puppeteer.laucnh  method-launches a new Chromium browser window.
 beforeAll(async () => {
   browser = await puppeteer.launch({
@@ -38,25 +35,27 @@ describe("Todo app", () => {
     });
   });
 
-  it("should be correct title", async () => {
-    const title = await page.title();
-    expect(title).toBe("E2E v.1");
-  });
+  describe('GROUP', () => {
+    it("should be correct title", async () => {
+      const title = await page.title();
+      expect(title).toBe("E2E v.1");
+    });
 
-  it("should add todo item", async () => {
-    const $$liElements1 = await page.$$(`[data-e2e=todo-items] li`);
-    expect($$liElements1.length).toBe(3);
-    // type text
-    await page.type("input.form-control.add-todo", "My New Task");
-    // simulate keyboard enter
-    await page.keyboard.type(String.fromCharCode(13));
-    // wait a bit
-    await page.waitFor(100);
-    // expect that we have 4 item
-    const $myList = await page.$(`[data-e2e=todo-items]`);
-    $myList.screenshot({ path: "list.png" });
-    const $$liElements2 = await page.$$(`[data-e2e=todo-items] li`);
-    expect($$liElements2.length).toBe(4);
+    it("should add todo item", async () => {
+      const $$liElements1 = await page.$$(`[data-e2e=todo-items] li`);
+      expect($$liElements1.length).toBe(3);
+      // type text
+      await page.type("input.form-control.add-todo", "My New Task");
+      // simulate keyboard enter
+      await page.keyboard.type(String.fromCharCode(13));
+      // wait a bit
+      await page.waitFor(100);
+      // expect that we have 4 item
+      const $myList = await page.$(`[data-e2e=todo-items]`);
+      $myList.screenshot({ path: "list.png" });
+      const $$liElements2 = await page.$$(`[data-e2e=todo-items] li`);
+      expect($$liElements2.length).toBe(4);
+    });
   });
 
   it("test completed action", async () => {
@@ -72,12 +71,10 @@ describe("Todo app", () => {
       (el) => el.textContent
     );
     expect(noItemsText).toBe("There are no items.");
-    // click All
-    // select first in list
-    // goto completed
-    // should be one element there
+
     await page.waitFor(5000);
   });
+
   it("should check items left", async () => {
     //type text
     await page.type("input.form-control.add-todo", "My New Task");
@@ -89,87 +86,9 @@ describe("Todo app", () => {
     expect("4 items left").toMatch("4 items left");
   });
 
-  //click All
-  //choose second item from All
-  //click icon Active
-  //in Active shouldn't be selected item
-  it("should check item in Active", async () => {
-    const $ActiveButton = await page.$("[data-e2e=active]");
-    await $ActiveButton.click();
-    await page.evaluate(() => {
-      const secondItem = await page.$$("[list-group-item]");
-      await secondItem[2].click();
-    });
+  //go to List
+  //delete second item
+  //confirm deletion
+  //expected there 
 
-    await page.waitFor(5000);
-  });
-
-  /*
-=======
-  });
-
-  it("test completed action", async () => {
-    // click completed
-    const $completedButton = await page.$("[data-e2e=completed]");
-    await $completedButton.click();
-
-    // see `There are no items.`
-    const noItemsSelector = ".alert.alert-info";
-    await page.$(".alert.alert-info");
-    const noItemsText = await page.$eval(
-      noItemsSelector,
-      (el) => el.textContent
-    );
-    expect(noItemsText).toBe("There are no items.");
-    // click All
-    // select first in list
-    // goto completed
-    // should be one element there
-    await page.waitFor(5000);
-  });
-  it("should check items left", async () => {
-    //type text
-    await page.type("input.form-control.add-todo", "My New Task");
-    //press enter
-    await page.keyboard.type(ENTER);
-    //grab selector of
-    const selector = "footer > div:nth-child(2)";
-    //verify he text matches 4
-    expect().toMatch("4 items left");
-  });
-
-  /*
-
->>>>>>> 831022152c23f89e3c8b54dc383d097e27d46793
-  it("type text", async () => {
-    //===========SETUP pupetter
-    // Accept confirm dialog
-    page.on("dialog", async (dialog) => {
-      await dialog.accept();
-    });
-    // type post
-    await page.type("textarea.form-control", "Hello World");
-    // click add
-    await page.click("button.btn-primary");
-    const delBtnSelector = "button.posts-list_remBtn__xNP6W";
-    // wait for  $del
-    await page.waitForSelector(delBtnSelector);
-    let $$delButtons = await page.$$(delBtnSelector);
-    // verify that 1 was added
-    // VERFY
-    expect($$delButtons.length).toBe(1);
-    // delete
-    await page.click(delBtnSelector);
-    await page.waitFor(300);
-    $$delButtons = await page.$$(delBtnSelector);
-    expect($$delButtons.length).toBe(0);
-  });
-
-  it("should check content label", async () => {
-    const $labelContent = await page.$eval(
-      `label.form-label`,
-      (el) => el.textContent
-    );
-    expect($labelContent).toBe("Content:");
-  });*/
 });
